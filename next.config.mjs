03ef7b203aa5416abe4b22a2c3d820a7c11e1bbd
ayush <usersynax@gmail.com>
2026-08-10 14:49:20 +0530

    Configure Images hosts

--- next.config.mjs
+++ next.config.mjs
@@ -30,7 +30,21 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "images.unsplash.com",
+            }, {
+                protocol: "https",
+                hostname: "api.dicebear.com",
             },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com"
+            }, {
+                protocol: "https",
+                hostname: "res.cloudinary.com"
+            },
+            {
+                protocol: "https",
+                hostname: "picsum.photos"
+            }
         ],
     },
     async headers() {
